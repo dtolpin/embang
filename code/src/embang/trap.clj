02a1671fc09aca7f4812a2025d65b1752ea91365
David Tolpin <david.tolpin@gmail.com>
2015-04-05 13:56:20 +0300

    singular in declaration types

--- code/src/embang/trap.clj
+++ code/src/embang/trap.clj
@@ -449,10 +449,10 @@
     (if (declaration? expr)
       (let [[_ kwd & args] expr]
         (case kwd
-          :primitive-namespaces (adding-primitive-namespaces args
-                                  (cps-of-do exprs cont))
-          :primitive-procedures (adding-primitive-procedures args
-                                  (cps-of-do exprs cont))
+          :primitive-namespace (adding-primitive-namespaces args
+                                 (cps-of-do exprs cont))
+          :primitive-procedure (adding-primitive-procedures args
+                                 (cps-of-do exprs cont))
           (assert false (format "Unknown declaration %s" expr))))
       (cps-of-expression
         expr
