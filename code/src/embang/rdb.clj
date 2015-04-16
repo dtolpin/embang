04dc752746fff91985181cd247e5f88c7bb63720
David Tolpin <david.tolpin@gmail.com>
2015-04-16 21:48:11 +0100

    fixed rdb

--- code/src/embang/rdb.clj
+++ code/src/embang/rdb.clj
@@ -1,6 +1,7 @@
 (ns embang.rdb
   "Random DB, same as Lightweight Metropolis-Hastings"
   (:refer-clojure :exclude [rand rand-int rand-nth])
+  (:require embang.lmh)
   (:use embang.inference))
 
 ;;;; Alias for Single-site Metropolis-Hastings
