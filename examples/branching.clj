b9a7e3e769dfbe75359675bc42ab797c02a80e6f
David Tolpin <david.tolpin@gmail.com>
2015-05-25 02:08:01 +0100

    converted branching

--- examples/branching.clj
+++ examples/branching.clj
@@ -1,33 +1,33 @@
-(ns branching
-  (:use [embang emit runtime]))
-
-;;; Simple branching
-
-(defanglican branching
-  "simple branching"
-  [assume fib (lambda (n)
-                      (cond ((= n 0) 0)
-                            ((= n 1) 1)
-                            (else (+ (fib (- n 1))
-                                     (fib (- n 2))))))]
-  [assume r (sample (poisson 4))]
-  [assume l (if (< 4 r)
-              6
-              (+ (fib (* 3 r)) (sample (poisson 4))))]
-  [observe (poisson l) 6]
-  [predict r])
-
-(defanglican optimized
-  "simple branching with some basic manual optimizations"
-  [assume poisson-source (poisson 4)]
-  [assume fib (lambda (n)
-                      (cond ((= n 0) 0)
-                            ((= n 1) 1)
-                            (else (+ (fib (- n 1))
-                                     (fib (- n 2))))))]
-  [assume r (sample poisson-source)]
-  [assume l (if (< 4 r)
-              6
-              (+ (fib (* 3 r)) (sample poisson-source)))]
-  [observe (poisson l) 6]
-  [predict r])
+(ns branching (:use [embang emit runtime]))
+(defquery
+ branching
+ "simple branching"
+ (let
+  [fib
+   (fn
+    fib
+    [n]
+    (cond (= n 0) 0 (= n 1) 1 :else (+ (fib (- n 1)) (fib (- n 2)))))]
+  (let
+   [r (sample (poisson 4))]
+   (let
+    [l (if (< 4 r) 6 (+ (fib (* 3 r)) (sample (poisson 4))))]
+    (observe (poisson l) 6)
+    (predict 'r r)))))
+(defquery
+ optimized
+ "simple branching with some basic manual optimizations"
+ (let
+  [poisson-source (poisson 4)]
+  (let
+   [fib
+    (fn
+     fib
+     [n]
+     (cond (= n 0) 0 (= n 1) 1 :else (+ (fib (- n 1)) (fib (- n 2)))))]
+   (let
+    [r (sample poisson-source)]
+    (let
+     [l (if (< 4 r) 6 (+ (fib (* 3 r)) (sample poisson-source)))]
+     (observe (poisson l) 6)
+     (predict 'r r))))))
