e80f2ea4036c61e0f6825b5df3bfe64c97e89dd1
David Tolpin <david.tolpin@gmail.com>
2015-05-28 22:11:15 +0100

    readable project.clj

--- code/project.clj
+++ code/project.clj
@@ -1,8 +1,30 @@
 (defproject embang "1.0.0-SNAPSHOT"
+  ;; Attributes for humans and repositories
   :description "m!, a probabilistic programming system"
   :url "http://bitbucket.org/dtolpin/embang"
   :license {:name "GNU General Public License Version 3"
             :url "http://www.gnu.org/licenses/gpl.html"}
+  :scm {:name "git"
+        :url "https://bitbucket.org/dtolpin/embang"}
+  :pom-addition [:developers [:developer
+                              [:name "David Tolpin"]
+                              [:url "http://offtopia.net/"]
+                              [:email "dvd@offtopia.net"]
+                              [:timezone "+2"]]]
+
+  ;; Deployment
+  :deploy-branches ["master"]
+  ;; Deploying to Maven Central; project name must be changed 
+  ;; to net.offtopia/embang
+  :deploy-repositories 
+  [["releases"
+    {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+     :creds :gpg}]
+   ["snapshots"
+    {:url "https://oss.sonatype.org/content/repositories/snapshots/"
+     :creds :gpg}]]
+
+  ;; Dependencies
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/data.json "0.2.6"]
@@ -12,24 +34,14 @@
                  [net.mikera/vectorz-clj "0.29.0"]]
   :plugins [[codox "0.8.11"]
             [dtolpin/lein-gorilla "0.3.5-SNAPSHOT"]]
-  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                                     :creds :gpg}]
-                         ["snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                      :creds :gpg}]]
-  :scm {:name "git"
-        :url "https://bitbucket.org/dtolpin/embang"}
-  :pom-addition [:developers [:developer
-                              [:name "David Tolpin"]
-                              [:url "http://offtopia.net/"]
-                              [:email "dvd@offtopia.net"]
-                              [:timezone "+2"]]]
+
+  ;; Run options
   :repl-options {:init-ns embang.core
                  :timeout 600000}
   :jvm-opts ["-Xmx1024m" "-Xss1024k"]
   :main ^:skip-aot embang.core
   :target-path "target/%s"
-  :resource-paths ["../examples"]
-  :profiles {:jar {:aot :all}
+  :profiles {:repl {:resource-paths ["../examples"]}
+             :jar {:aot :all}
              :uberjar {:aot :all}}
-  :deploy-branches ["master"]
   :aliases {"publish" ["do" ["clean"] ["test"] ["uberjar"]]})
